6c5915762099985b68cddf8d8905661f62f15a4b
Cedric Wang <cedwang@amazon.com>
2025-04-01 05:44:55 +0000

    commit mar 31

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -199,6 +199,7 @@ return {
       -- gopls = {},
       -- pyright = {},
       -- rust_analyzer = {},
+      tsserver = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
