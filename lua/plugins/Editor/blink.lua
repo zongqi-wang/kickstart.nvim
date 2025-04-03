b125ebc78b442bf216705693f1cb54816d8e24e7
Cedric Wang <cedwang@amazon.com>
2025-04-03 20:22:46 +0000

    fix blink

--- lua/plugins/Editor/blink.lua
+++ lua/plugins/Editor/blink.lua
@@ -1,279 +1,206 @@
--- completion plugin with support for LSPs and external sources that updates
--- on every keystroke with minimal overhead
-
--- https://www.lazyvim.org/extras/coding/blink
--- https://github.com/saghen/blink.cmp
--- Documentation site: https://cmp.saghen.dev/
-
--- NOTE: Specify the trigger character(s) used for luasnip
-local trigger_text = ";"
-
 return {
-  "saghen/blink.cmp",
-  enabled = true,
-  -- In case there are breaking changes and you want to go back to the last
-  -- working release
-  -- https://github.com/Saghen/blink.cmp/releases
-  -- version = "v0.13.1",
-  dependencies = {
-    "moyiz/blink-emoji.nvim",
-    "Kaiser-Yang/blink-cmp-dictionary",
-  },
-  opts = function(_, opts)
-    -- I noticed that telescope was extremeley slow and taking too long to open,
-    -- assumed related to blink, so disabled blink and in fact it was related
-    -- :lua print(vim.bo[0].filetype)
-    -- So I'm disabling blink.cmp for Telescope
-    opts.enabled = function()
-      -- Get the current buffer's filetype
-      local filetype = vim.bo[0].filetype
-      -- Disable for Telescope buffers
-      if filetype == "TelescopePrompt" or filetype == "minifiles" or filetype == "snacks_picker_input" then
-        return false
-      end
-      return true
-    end
+  -- Autocompletion Engine
+  {
+    "saghen/blink.cmp",
+    event = "InsertEnter", -- Load dependency plugins correctly and on InsertEnter
+    dependencies = {
+      -- Snippet Engine & Snippets sources
+      {
+        "L3MON4D3/LuaSnip",
+        build = (not vim.uv.os_uname().sysname:find("Windows"))
+            and "make install_jsregexp"
+          or nil, -- Recommended build step for regex support in snippets
+        dependencies = {
+          -- Collection of common snippets
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+            -- You can add custom snippet directories here if needed:
+            -- require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
+          end,
+        },
+      },
+      -- Optional: If you want snippet integration beyond basic preset,
+      -- though blink.cmp might handle basic luasnip integration via preset
+      -- "moyiz/blink-luasnip.nvim",
 
-    -- NOTE: The new way to enable LuaSnip
-    -- Merge custom sources with the existing ones from lazyvim
-    -- NOTE: by default lazyvim already includes the lazydev source, so not adding it here again
-    opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-      default = { "lsp", "path", "snippets", "buffer", "dadbod", "emoji", "dictionary" },
-      providers = {
-        lsp = {
-          name = "lsp",
-          enabled = true,
-          module = "blink.cmp.sources.lsp",
-          kind = "LSP",
-          min_keyword_length = 2,
-          -- When linking markdown notes, I would get snippets and text in the
-          -- suggestions, I want those to show only if there are no LSP
-          -- suggestions
-          --
-          -- Enabled fallbacks as this seems to be working now
-          -- Disabling fallbacks as my snippets wouldn't show up when editing
-          -- lua files
-          -- fallbacks = { "snippets", "buffer" },
-          score_offset = 90, -- the higher the number, the higher the priority
-        },
-        path = {
-          name = "Path",
-          module = "blink.cmp.sources.path",
-          score_offset = 25,
-          -- When typing a path, I would get snippets and text in the
-          -- suggestions, I want those to show only if there are no path
-          -- suggestions
-          fallbacks = { "snippets", "buffer" },
-          -- min_keyword_length = 2,
-          opts = {
-            trailing_slash = false,
-            label_trailing_slash = true,
-            get_cwd = function(context)
-              return vim.fn.expand(("#%d:p:h"):format(context.bufnr))
-            end,
-            show_hidden_files_by_default = true,
+      -- Other potential sources (add if needed, like path, emoji, etc.)
+      "hrsh7th/cmp-path", -- If you want path completion
+      "hrsh7th/cmp-buffer", -- If you want buffer text completion
+      -- Add other source plugins like emoji, dictionary etc. if you were using them
+      -- "moyiz/blink-emoji.nvim",
+      -- "Kaiser-Yang/blink-cmp-dictionary",
+    },
+    opts = function()
+      local cmp = require("blink.cmp")
+      local types = require("blink.cmp.config.types") -- Useful for keymap actions
+
+      return {
+        -- === Enable/Disable ===
+        enabled = function()
+          -- Disable completion in specific buffers like Telescope, etc.
+          local buftype = vim.bo.buftype
+          if buftype == "prompt" or buftype == "nofile" then
+            return false
+          end
+          -- Example: Disable in comments (requires nvim-treesitter context)
+          -- local context = require("cmp.config.context")
+          -- if context.isInSyntaxGroup("Comment") then return false end
+          return true
+        end,
+
+        -- === Sources ===
+        -- List of sources active by default for all filetypes
+        sources = {
+          default = {
+            "lsp",
+            "snippets", -- Or "luasnip" if using blink-luasnip directly
+            "buffer",
+            "path",
+            -- Add other sources like "emoji", "dictionary" here if installed/enabled
           },
-        },
-        buffer = {
-          name = "Buffer",
-          enabled = true,
-          max_items = 3,
-          module = "blink.cmp.sources.buffer",
-          min_keyword_length = 2,
-          score_offset = 15, -- the higher the number, the higher the priority
-        },
-        snippets = {
-          name = "snippets",
-          enabled = true,
-          max_items = 15,
-          min_keyword_length = 2,
-          module = "blink.cmp.sources.snippets",
-          score_offset = 85, -- the higher the number, the higher the priority
-          -- Only show snippets if I type the trigger_text characters, so
-          -- to expand the "bash" snippet, if the trigger_text is ";" I have to
-          should_show_items = function()
-            local col = vim.api.nvim_win_get_cursor(0)[2]
-            local before_cursor = vim.api.nvim_get_current_line():sub(1, col)
-            -- NOTE: remember that `trigger_text` is modified at the top of the file
-            return before_cursor:match(trigger_text .. "%w*$") ~= nil
-          end,
-          -- After accepting the completion, delete the trigger_text characters
-          -- from the final inserted text
-          -- Modified transform_items function based on suggestion by `synic` so
-          -- that the luasnip source is not reloaded after each transformation
-          -- https://github.com/linkarzu/dotfiles-latest/discussions/7#discussion-7849902
-          -- NOTE: I also tried to add the ";" prefix to all of the snippets loaded from
-          -- friendly-snippets in the luasnip.lua file, but I was unable to do
-          -- so, so I still have to use the transform_items here
-          -- This removes the ";" only for the friendly-snippets snippets
-          transform_items = function(_, items)
-            local line = vim.api.nvim_get_current_line()
-            local col = vim.api.nvim_win_get_cursor(0)[2]
-            local before_cursor = line:sub(1, col)
-            local start_pos, end_pos = before_cursor:find(trigger_text .. "[^" .. trigger_text .. "]*$")
-            if start_pos then
-              for _, item in ipairs(items) do
-                if not item.trigger_text_modified then
-                  ---@diagnostic disable-next-line: inject-field
-                  item.trigger_text_modified = true
-                  item.textEdit = {
-                    newText = item.insertText or item.label,
-                    range = {
-                      start = { line = vim.fn.line(".") - 1, character = start_pos - 1 },
-                      ["end"] = { line = vim.fn.line(".") - 1, character = end_pos },
-                    },
-                  }
-                end
-              end
-            end
-            return items
-          end,
-        },
-        -- Example on how to configure dadbod found in the main repo
-        -- https://github.com/kristijanhusak/vim-dadbod-completion
-        dadbod = {
-          name = "Dadbod",
-          module = "vim_dadbod_completion.blink",
-          min_keyword_length = 2,
-          score_offset = 85, -- the higher the number, the higher the priority
-        },
-        -- https://github.com/moyiz/blink-emoji.nvim
-        emoji = {
-          module = "blink-emoji",
-          name = "Emoji",
-          score_offset = 93, -- the higher the number, the higher the priority
-          min_keyword_length = 2,
-          opts = { insert = true }, -- Insert emoji (default) or complete its name
-        },
-        -- https://github.com/Kaiser-Yang/blink-cmp-dictionary
-        -- In macOS to get started with a dictionary:
-        -- cp /usr/share/dict/words ~/github/dotfiles-latest/dictionaries/words.txt
-        --
-        -- NOTE: For the word definitions make sure "wn" is installed
-        -- brew install wordnet
-        dictionary = {
-          module = "blink-cmp-dictionary",
-          name = "Dict",
-          score_offset = 20, -- the higher the number, the higher the priority
-          -- https://github.com/Kaiser-Yang/blink-cmp-dictionary/issues/2
-          enabled = true,
-          max_items = 8,
-          min_keyword_length = 3,
-          opts = {
-            -- -- The dictionary by default now uses fzf, make sure to have it
-            -- -- installed
-            -- -- https://github.com/Kaiser-Yang/blink-cmp-dictionary/issues/2
-            --
-            -- Do not specify a file, just the path, and in the path you need to
-            -- have your .txt files
-            dictionary_directories = { vim.fn.expand("~/github/dotfiles-latest/dictionaries") },
-            -- Notice I'm also adding the words I add to the spell dictionary
-            dictionary_files = {
-              vim.fn.expand("~/github/dotfiles-latest/neovim/neobean/spell/en.utf-8.add"),
-              vim.fn.expand("~/github/dotfiles-latest/neovim/neobean/spell/es.utf-8.add"),
+          -- Configuration for individual source providers
+          providers = {
+            lsp = {
+              name = "LSP",
+              module = "blink.cmp.sources.lsp",
+              enabled = true,
+              kind = " LSP", -- Customize the kind display
+              score_offset = 90, -- High priority
+              min_keyword_length = 1,
             },
-            -- --  NOTE: To disable the definitions uncomment this section below
-            --
-            -- separate_output = function(output)
-            --   local items = {}
-            --   for line in output:gmatch("[^\r\n]+") do
-            --     table.insert(items, {
-            --       label = line,
-            --       insert_text = line,
-            --       documentation = nil,
-            --     })
-            --   end
-            --   return items
-            -- end,
+            snippets = {
+              name = "Snippets",
+              module = "blink.cmp.sources.snippets", -- Use built-in snippets source
+              enabled = true,
+              kind = " Snippets",
+              score_offset = 80,
+            },
+            buffer = {
+              name = "Buffer",
+              module = "blink.cmp.sources.buffer",
+              enabled = true,
+              kind = " Buffer",
+              score_offset = 50,
+              min_keyword_length = 3,
+              max_items = 5,
+            },
+            path = {
+              name = "Path",
+              module = "blink.cmp.sources.path",
+              enabled = true,
+              kind = " Path",
+              score_offset = 60,
+            },
+            -- Configure other providers (emoji, dictionary) if you add them
           },
         },
-        -- -- Third class citizen mf always talking shit
-        -- copilot = {
-        --   name = "copilot",
-        --   enabled = true,
-        --   module = "blink-cmp-copilot",
-        --   kind = "Copilot",
-        --   min_keyword_length = 6,
-        --   score_offset = -100, -- the higher the number, the higher the priority
-        --   async = true,
-        -- },
-      },
-    })
 
-    opts.cmdline = {
-      enabled = true,
-    }
+        -- === Keybindings ===
+        -- IMPORTANT: Define mappings directly to avoid preset issues
+        keymap = {
+          mapping = {
+            -- Snippet Navigation (requires LuaSnip and integration)
+            ["<Tab>"] = { "i", "s" }, -- Try snippet first in insert/select modes
+            ["<S-Tab>"] = { "i", "s" }, -- Try snippet backward first
 
-    opts.completion = {
-      -- accept = {
-      --   auto_brackets = {
-      --     enabled = true,
-      --     default_brackets = { ";", "" },
-      --     override_brackets_for_filetypes = {
-      --       markdown = { ";", "" },
-      --     },
-      --   },
-      -- },
-      --   keyword = {
-      --     -- 'prefix' will fuzzy match on the text before the cursor
-      --     -- 'full' will fuzzy match on the text before *and* after the cursor
-      --     -- example: 'foo_|_bar' will match 'foo_' for 'prefix' and 'foo__bar' for 'full'
-      --     range = "full",
-      --   },
-      menu = {
-        border = "single",
-      },
-      documentation = {
-        auto_show = true,
-        window = {
-          border = "single",
+            -- Completion Selection
+            ["<Up>"] = { "select_prev", "fallback" },
+            ["<Down>"] = { "select_next", "fallback" },
+            ["<C-p>"] = { "select_prev", "fallback" },
+            ["<C-n>"] = { "select_next", "fallback" },
+
+            -- Documentation Scrolling
+            ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
+            ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
+            ["<C-k>"] = { "scroll_documentation_up", "fallback" }, -- Alternative
+            ["<C-j>"] = { "scroll_documentation_down", "fallback" }, -- Alternative
+
+            -- Completion Control
+            ["<C-Space>"] = { "complete", "fallback" }, -- Manually trigger
+            ["<C-e>"] = { "abort", "fallback" }, -- Close menu
+
+            -- Confirmation
+            ["<CR>"] = { "confirm", { select = true } }, -- Confirm selection (select = true helps confirm implicit selection)
+            -- ["<C-y>"] = { "confirm", "fallback" }, -- Optional alternative confirm
+          },
+          -- DO NOT use 'preset = "default"' here, define 'mapping' instead
         },
-      },
-    }
 
-    -- opts.fuzzy = {
-    --   -- Disabling this matches the behavior of fzf
-    --   use_typo_resistance = false,
-    --   -- Frecency tracks the most recently/frequently used items and boosts the score of the item
-    --   use_frecency = true,
-    --   -- Proximity bonus boosts the score of items matching nearby words
-    --   use_proximity = false,
-    -- }
+        -- === Appearance (Completion Menu & Documentation) ===
+        completion = {
+          menu = {
+            border = "single", -- Or "rounded", "double", "shadow", etc.
+            -- You can customize item kinds, formatting here if desired
+            -- format_item = function(entry) ... end
+          },
+          documentation = {
+            auto_show = true,
+            window = {
+              border = "single",
+            },
+          },
+          keyword = {
+            -- 'prefix' matches only text before cursor
+            -- 'full' matches text before and after cursor
+            range = "prefix",
+          },
+        },
 
-    opts.snippets = {
-      preset = "luasnip", -- Choose LuaSnip as the snippet engine
-    }
+        -- === Snippet Engine Integration ===
+        snippets = {
+          preset = "luasnip", -- Tell blink.cmp to use LuaSnip
+          -- Configure snippet source options if needed (using the 'providers.snippets' table above is preferred)
+          -- opts = { use_show_condition = true, show_autosnippets = true }
+        },
 
-    -- -- To specify the options for snippets
-    -- opts.sources.providers.snippets.opts = {
-    --   use_show_condition = true, -- Enable filtering of snippets dynamically
-    --   show_autosnippets = true, -- Display autosnippets in the completion menu
-    -- }
+        -- === Fuzzy Matching (Optional Customization) ===
+        -- fuzzy = {
+        --   use_typo_resistance = false,
+        --   use_frecency = true,
+        --   use_proximity = false,
+        -- },
 
-    -- The default preset used by lazyvim accepts completions with enter
-    -- I don't like using enter because if on markdown and typing
-    -- something, but you want to go to the line below, if you press enter,
-    -- the completion will be accepted
-    -- https://cmp.saghen.dev/configuration/keymap.html#default
-    -- opts.keymap = {
-    --   mapping = {
-    --     ["<Tab>"] = { "snippet_forward", "fallback" },
-    --     ["<S-Tab>"] = { "snippet_backward", "fallback" },
-    --
-    --     ["<Up>"] = { "select_prev", "fallback" },
-    --     ["<Down>"] = { "select_next", "fallback" },
-    --     ["<C-p>"] = { "select_prev", "fallback" },
-    --     ["<C-n>"] = { "select_next", "fallback" },
-    --
-    --     ["<S-k>"] = { "scroll_documentation_up", "fallback" },
-    --     ["<S-j>"] = { "scroll_documentation_down", "fallback" },
-    --
-    --     ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-    --     ["<C-e>"] = { "hide", "fallback" },
-    --
-    --     }
-    --         }
+        -- === Command Line Completion ===
+        cmdline = {
+          enabled = true,
+          sources = {
+            default = { "cmdline", "path", "buffer" },
+            providers = {
+              cmdline = {
+                name = "Cmdline",
+                module = "blink.cmp.sources.cmdline",
+                enabled = true,
+              },
+            },
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      -- Explicitly call setup - Good practice especially if opts function does complex things
+      require("blink.cmp").setup(opts)
+      print("blink.cmp setup called.") -- Add print statement for debugging
+    end,
+  },
 
-    return opts
-  end,
+  -- Ensure LSP config is loaded *before* cmp if not handled by lazy.nvim dependencies automatically
+  -- Example: { "neovim/nvim-lspconfig", dependencies = { "mason.nvim" } }
+
+  -- Optional but recommended: UI enhancements for LSP/Completion
+  {
+    "nvimdev/lsp-rainbow.nvim", -- Colorizes matching brackets based on LSP scope
+    event = "LspAttach",
+  },
+  {
+    "utilyre/sentiment.nvim", -- Enhanced diagnostic visualization
+    event = "LspAttach",
+    opts = {},
+    config = true, -- Runs require('sentiment').setup({})
+  },
+  {
+    "folke/trouble.nvim", -- Better diagnostics list
+    cmd = { "TroubleToggle", "Trouble" },
+    opts = { use_diagnostic_signs = true },
+  },
 }
